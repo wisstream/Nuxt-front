bc959d923ff628f0acba6f617545b4267560f4a5
wisstream <loukil_w@etna-alternance.net>
2024-03-18 17:18:40 +0100

    Create [...slug].vue

---
+++ pages/personnes/[...slug].vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+
+/*import type { Player } from "~/models/player.model",*/
+
+const {findOne} = useStrapi4()
+const route = useRoute()
+
+const {data: player} =useAsyncData('players', 
+() => findOne<{data: Player[]}> (`player/${route.params.slug}`)
+)
+
+</script>
+<template>
+  <pre>{{ player }}</pre>
+</template>
